0ed3b94ebfdbcafa531da52817dbf7a9476f28aa
shaun <shaun@LAINGYY>
2024-01-06 19:31:35 +0000

    Add project files.

---
+++ CloneMinecraft/Game.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace CloneMinecraft
+{
+    class Game: GameWindow
+    {
+        float[] verticies =
+        {
+            0f, 0.5f, 0f, //top vertex
+            -0.5f, -0.5f, 0f, //bottom left
+            0.5f, -0.5f, 0f //bottom right
+        };
+
+        // RenderPipeline Variables
+        int vao;
+        int shaderProgram;
+
+        // is the screeen width and height
+        int width;
+        int height;
+
+        public Game(int width, int height) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            this.width = width;
+            this.height = height;   
+
+            // Centres the program to the monitor
+            this.CenterWindow(new Vector2i(width, height));
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
+            this.width = e.Width;
+            this.height = e.Height;
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            vao = GL.GenVertexArray();
+
+            int vbo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, verticies.Length * sizeof(float), verticies, BufferUsageHint.StaticDraw);
+
+            //Bind vao
+            GL.BindVertexArray(vao);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexArrayAttrib(vao, 0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0); //unbinds buffer
+            GL.BindVertexArray(0); //unbinds vao
+
+            // Create Shader Program
+            shaderProgram = GL.CreateProgram();
+
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, LoadShaderSource("Default.vert"));
+            GL.CompileShader(vertexShader);
+
+            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, LoadShaderSource("Default.frag"));
+            GL.CompileShader(fragmentShader);
+
+            GL.AttachShader(shaderProgram, vertexShader);
+            GL.AttachShader(shaderProgram, fragmentShader);
+
+            GL.LinkProgram(shaderProgram);
+
+            //delete shaders
+            GL.DeleteShader(vertexShader); 
+            GL.DeleteShader(fragmentShader);
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+            GL.DeleteVertexArray(vao);
+            GL.DeleteProgram(shaderProgram);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(0.6f, 0.3f, 1f, 1f);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            // draw a triangle
+            GL.UseProgram(shaderProgram);
+            GL.BindVertexArray(vao);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+
+
+            Context.SwapBuffers();
+
+            base.OnRenderFrame(args);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        // Function to load a text file and return its contents as a string
+        public static string LoadShaderSource(string filePath)
+        {
+            string shaderSource = "";
+
+            try
+            {
+                using (StreamReader reader = new StreamReader("../../../Shaders/" + filePath))
+                {
+                    shaderSource = reader.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to load shader source file: " + e.Message);
+            }
+
+            return shaderSource;
+        }
+    }
+}
---
+++ CloneMinecraft/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CloneMinecraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // creates a game object and disposes it after leaving the scope
+            using(Game game = new Game(500, 500))
+            {
+                game.Run();
+            }
+        }
+    }
+}
